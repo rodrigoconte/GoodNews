1f1b88f601edb7a74986d72378e5cfa71a4cd481
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-04-20 07:55:23 -0400

    [Feature] Includes navigation bar and removed some files not used
    This commit is to include global navigation bar with prefered appearance

--- GoodNews/GoodNews/Base/SceneDelegate.swift
+++ GoodNews/GoodNews/Base/SceneDelegate.swift
@@ -8,21 +8,32 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-                let window = UIWindow(windowScene: windowScene)
-                
-                window.rootViewController = NewsTableViewController()
-                window.makeKeyAndVisible()
-                
-                self.window = window
-
+            
+        let window = UIWindow(windowScene: windowScene)
+        let navController = UINavigationController(rootViewController: NewsTableViewController())
+        
+        self.window = window
+        
+        let navBar = UINavigationBarAppearance()
+        navBar.configureWithOpaqueBackground()
+        navBar.backgroundColor = .gray
+        navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        navController.navigationBar.standardAppearance = navBar
+        navController.navigationBar.scrollEdgeAppearance = navBar
+        
+        self.window!.rootViewController = navController
+        self.window?.makeKeyAndVisible()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
 }
 
