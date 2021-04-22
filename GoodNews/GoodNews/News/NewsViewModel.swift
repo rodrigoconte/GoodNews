474faec2146dd3e3eed437ba98ce878ac2af2b8d
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-04-22 19:51:41 -0400

    [Feature] Add articles view model do be used on News View Controller
    This commit delivery articles list and article view model to send data from model to view controller

--- GoodNews/GoodNews/News/NewsViewModel.swift
+++ GoodNews/GoodNews/News/NewsViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+struct NewsViewModel {
+    private let article: Article
+}
+
+extension NewsViewModel {
+    
+    init(_ article: Article) {
+        self.article = article
+    }
+    
+}
+
+extension NewsViewModel {
+    
+    var title: String {
+        return self.article.title
+    }
+    
+    var description: String {
+        return self.article.articleDescription ?? "-"
+    }
+    
+}
+
+
+struct ArticlesListViewModel {
+    let articles: [Article]
+}
+
+extension ArticlesListViewModel {
+    
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func rowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> NewsViewModel {
+        let article = self.articles[index]
+        return NewsViewModel(article)
+    }
+    
+}
